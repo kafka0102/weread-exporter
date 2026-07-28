f5617def64ea55d0fd3f66f18fa0d6eabf3e82ed
yujianjia <yujianjia@lixiang.com>
2026-07-28 09:35:19 +0800

    chore: 将章间每两千字等待默认改为 0.5 秒

--- tests/test_env_config.py
+++ tests/test_env_config.py
@@ -60,8 +60,8 @@ class TestEnvConfig(unittest.TestCase):
         # 代码默认值（不依赖当前 .env 是否被改过）
         self.assertEqual(env_config.env_float("SLEEP_BOOK_DETAIL_INTERVAL_UNSET_X", 5.0), 5.0)
         self.assertEqual(env_config.SLEEP_BOOK_INTERVAL, 60.0)
-        self.assertEqual(env_config.SLEEP_CHAPTER_PER_2K_CHARS, 1.0)
-        self.assertEqual(env_config.SLEEP_CHAPTER_MIN, 1.0)
+        self.assertEqual(env_config.SLEEP_CHAPTER_PER_2K_CHARS, 0.5)
+        self.assertEqual(env_config.SLEEP_CHAPTER_MIN, 0.3)
         self.assertEqual(env_config.SLEEP_CHAPTER_MAX, 2.0)
 
 
